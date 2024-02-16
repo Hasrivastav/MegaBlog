e8a98aa64abe58a123a464f609b8c47d2bada325
HARSH SRIVASTAV <96014384+Hasrivastav@users.noreply.github.com>
2024-02-16 19:47:59 +0530

    Added apikey for RTE

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -13,6 +13,7 @@ export default function RTE({name, control, label, defaultValue =""}) {
     control={control}
     render={({field: {onChange}}) => (
         <Editor
+        apiKey='uc42zw7un77m14cajl8gx4a15a9o5vb84sxxbosy3fegtobj'
         initialValue={defaultValue}
         init={{
             initialValue: defaultValue,
